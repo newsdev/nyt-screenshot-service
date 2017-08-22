d0aa4424f829f0f19b01dd347d677d11c51863c3
Jeremy Bowers <jeremyjbowers@gmail.com>
2017-08-22 12:06:32 -0400

    It's fun how much modules make development easy [record scratch] you may be wondering how I got here.

--- screenshotter/app.py
+++ screenshotter/app.py
@@ -23,7 +23,7 @@ import peewee
 from peewee import *
 from pyiap.pyiap_flask_middleware import VerifyJWTMiddleware
 
-import utils
+import screenshotter.utils
 
 settings = importlib.import_module('config.%s.settings' % utils.get_env())
 app = Flask(__name__)
