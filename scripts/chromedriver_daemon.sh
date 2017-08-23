671f57294718cdff435b52983a7be00992c677da
Jeremy Bowers <jeremyjbowers@gmail.com>
2017-08-23 15:38:45 -0400

    I'm afraid I can't do that Dave.

--- scripts/chromedriver_daemon.sh
+++ scripts/chromedriver_daemon.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-/usr/src/app/scripts/setup_swap.sh                          # Resize swap to be bigger.
+# /usr/src/app/scripts/setup_swap.sh                          # Resize swap to be bigger.
 chromedriver                                                # Run the jewels. Run them jewels FAST.
