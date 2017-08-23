9b56fe7478d439ed9851938eddc89d6e90cf96ea
Jeremy Bowers <jeremyjbowers@gmail.com>
2017-08-23 15:30:31 -0400

    I continue to surprise myself.

--- scripts/setup_swap.sh
+++ scripts/setup_swap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo umount -l /dev/shm
-sudo umount -l /tmp
+umount -l /dev/shm
+umount -l /tmp
 
-sudo mount -t tmpfs -o size=1280m tmpfs /dev/shm
-sudo mount -t tmpfs -o size=768m tmpfs /tmp
+mount -t tmpfs -o size=1280m tmpfs /dev/shm
+mount -t tmpfs -o size=768m tmpfs /tmp
